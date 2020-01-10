233beadf3ecf37882dd09a982c742202386af22e
Anurag KOndeya <anurag.kondeya@gmail.com>
2020-01-11 08:27:13 +1100

    I am rich app

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Anurag Kondeya on 11/1/20.
+//  Copyright © 2020 Anurag Kondeya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
